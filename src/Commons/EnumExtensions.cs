bd53e35817cfb450e9563cd69bcba6b62c57b52c
questionmarkexclamationpoint <questionmarkexclamation@github.com>
2024-07-11 17:57:33 -0700

    add enum extensions test

--- src/Commons/EnumExtensions.cs
+++ src/Commons/EnumExtensions.cs
@@ -1,20 +1,19 @@
 namespace Commons;
 
 using System;
-using System.Runtime.CompilerServices;
 
 public static class EnumExtensions {
-    public static long ToLong(this Enum value)
+    public static long ToIndex(this Enum value)
             => Array.IndexOf(Enum.GetValues(value.GetType()), value);
 
-    public static long ToLong<TEnum>(TEnum value) where TEnum : Enum
+    public static long ToIndex<TEnum>(TEnum value) where TEnum : Enum
             => Array.IndexOf(Enum.GetValues(value.GetType()), value);
 
-    public static TEnum? FromLong<TEnum>(long value) where TEnum : struct, Enum {
+    public static TEnum? FromIndex<TEnum>(long index) where TEnum : struct, Enum {
         var values = Enum.GetValues<TEnum>();
-        if (value < 0 || value >= values.Length) {
+        if (index < 0 || index >= values.Length) {
             return null;
         }
-        return Unsafe.As<long, TEnum>(ref value);
+        return values[index];
     }
 }
---
+++ tests/CommonsTest/EnumExtensionsTest.cs
@@ -0,0 +1,28 @@
+namespace CommonsTest;
+
+using System;
+using Commons;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+[TestClass]
+public class EnumExtensionsTest {
+    private enum TestEnum {
+        A = -2,
+        B = -3,
+        C = 5,
+        D = 10
+    }
+
+    [TestMethod]
+    public void TestToAndFromLong() {
+        var values = Enum.GetValues<TestEnum>();
+        for (var i = 0; i < values.Length; i++) {
+            Assert.AreEqual(i, values[i].ToIndex(), $"{values[i]} failed");
+            Assert.AreEqual(
+                    values[i],
+                    EnumExtensions.FromIndex<TestEnum>(values[i].ToIndex()),
+                    $"{values[i]} failed"
+            );
+        }
+    }
+}
