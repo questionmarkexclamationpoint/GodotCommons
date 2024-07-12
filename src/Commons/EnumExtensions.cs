133283fbcc83ac816526137e86acd854ae192db2
questionmarkexclamationpoint <questionmarkexclamation@github.com>
2024-07-11 17:44:39 -0700

    EnumUtil -> EnumExtensions

--- src/Commons/EnumExtensions.cs
+++ src/Commons/EnumExtensions.cs
@@ -3,8 +3,12 @@ namespace Commons;
 using System;
 using System.Runtime.CompilerServices;
 
-public static class EnumUtil {
-    public static long ToLong<TEnum>(TEnum value) where TEnum : Enum => Unsafe.As<TEnum, long>(ref value);
+public static class EnumExtensions {
+    public static long ToLong(this Enum value)
+            => Array.IndexOf(Enum.GetValues(value.GetType()), value);
+
+    public static long ToLong<TEnum>(TEnum value) where TEnum : Enum
+            => Array.IndexOf(Enum.GetValues(value.GetType()), value);
 
     public static TEnum? FromLong<TEnum>(long value) where TEnum : struct, Enum {
         var values = Enum.GetValues<TEnum>();
