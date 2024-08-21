62733be03d02054549b8abeebe9a523355b2f442
questionmarkexclamationpoint <questionmarkexclamation@github.com>
2024-08-21 11:25:30 -0700

    add range methods for random extensions

--- src/Commons/Extensions/RandomExt.cs
+++ src/Commons/Extensions/RandomExt.cs
@@ -5,19 +5,27 @@ using System;
 public static class RandomExt {
     public static float NextFloat(this Random random) => random.NextSingle();
 
-
     public static double NextDoubleInRange(this Random random, double min, double max) {
         (min, max) = min > max ? (max, min) : (min, max);
         return (random.NextDouble() * (max - min)) + min;
     }
+
+    public static double NextDoubleInRange(this Random random, Range<double> range)
+        => random.NextDoubleInRange(range.Start, range.End);
 
     public static float NextFloatInRange(this Random random, float min, float max) {
         (min, max) = min > max ? (max, min) : (min, max);
         return (random.NextFloat() * (max - min)) + min;
     }
 
+    public static float NextFloatInRange(this Random random, Range<float> range)
+        => random.NextFloatInRange(range.Start, range.End);
+
     public static int NextIntInRange(this Random random, int min, int max) {
         (min, max) = min > max ? (max, min) : (min, max);
         return (int)random.NextInt64(max - min) + min;
     }
+
+    public static int NextIntInRange(this Random random, Range<int> range)
+        => random.NextIntInRange(range.Start, range.End);
 }
--- tests/Commons/Test/RandomExtTest.cs
+++ tests/Commons/Test/RandomExtTest.cs
@@ -6,11 +6,28 @@ using System;
 
 [TestClass]
 public class RandomExtTest {
+    private static void TestValues(Random random, double a, double b) {
+        // make variance half the difference, and expected the mean of a and b,
+        // so that any correctly generated random value will be in the expected range
+        var variance = Math.Abs(b - a) / 2;
+        var expected = (a + b) / 2;
+        var f = random.NextFloatInRange((float)a, (float)b);
+        Assert.AreEqual(expected, f, variance);
+        Assert.AreEqual(expected, random.NextFloatInRange(new((float)a, f, (int)b)), variance);
+        var i = random.NextIntInRange((int)a, (int)b);
+        Assert.AreEqual(expected, i, variance);
+        Assert.AreEqual(expected, random.NextIntInRange(new((int)a, i, (int)b)), variance);
+        var d = random.NextDoubleInRange(a, b);
+        Assert.AreEqual(expected, d, variance);
+        Assert.AreEqual(expected, random.NextDoubleInRange(new((int)a, d, (int)b)), variance);
+    }
+
     [TestMethod]
     public void TestRandom() {
         var random = new Random();
-        Assert.AreEqual(0, random.NextFloatInRange(0, 0));
-        Assert.AreEqual(0, random.NextIntInRange(0, 0));
-        Assert.AreEqual(0, random.NextDoubleInRange(0, 0));
+        TestValues(random, 0, 0);
+        TestValues(random, -100, 22.2);
+        TestValues(random, 100, -2222);
+        TestValues(random, 100, 30);
     }
 }
