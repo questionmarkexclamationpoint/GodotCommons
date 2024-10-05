56c755581729614db6b493ef3c3f9ffed2d2e380
questionmarkexclamationpoint <questionmarkexclamation@github.com>
2024-10-05 04:13:14 -0700

    remove unused interfaces

--- src/Commons/Graph/Generic/IComplexEdgeNode.cs
+++
@@ -1,7 +0,0 @@
-namespace QuestionMarkExclamationPoint.Commons.Graph.Generic;
-
-public interface IComplexEdgeNode<TNode, TIndex, TEdge>
-        : IIndexedNode<TNode, TEdge>
-        where TNode : IComplexEdgeNode<TNode, TIndex, TEdge> {
-    TEdge this[TIndex index] { get; }
-}
